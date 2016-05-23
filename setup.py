90ba10e19e39bbacc0d6d51d413feff75cbde9e8
Aykol <akoikelov@gmail.com>
2016-05-23 19:27:39 +0600

    + Added setup.py for pypi

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='akoikelov.djazz',
+    version='1.0',
+    packages=['akoikelov', 'akoikelov.djazz'],
+    url='https://github.com/akoikelov/djazz',
+    license='',
+    author='akoikelov',
+    author_email='kolyakoikelov@gmail.com',
+    description='Djazz is helper library for Django framework'
+)
