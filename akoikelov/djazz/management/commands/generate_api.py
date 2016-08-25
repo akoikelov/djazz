f85d929f491a81ecd09d3237a2d37bd9d80b06e9
Aykol <akoikelov@gmail.com>
2016-08-25 18:12:42 +0600

    ^ modified generate_api generator class (added urlpatters.append() construct) + added class Meta to model.py-tpl + added file and image field types to generator

--- akoikelov/djazz/management/commands/generate_api.py
+++ akoikelov/djazz/management/commands/generate_api.py
@@ -19,37 +19,21 @@ class Command(BaseCommand):
         models_names = list(a.name for a in pyclbr.readmodule(package + '.models').values())
 
         package_dir = os.path.join(os.getcwd(), package)
-        api_skeleton = open(os.path.join(akoikelov.djazz.__path__[0], 'conf', ) + '/tpl/api.py-tpl').read()
-        api_file_resource = open(package_dir + '/api.py', 'a')
-        urls = []
-        urls_skeleton = "    url(r'^api/', include(%sResource().urls)),\n"
-        url_data = ''
-
-        with open(os.path.join(os.getcwd(), main_package_name) + '/urls.py', 'r') as w:
-            data = w.readlines()
-
-        for line in data:
-            if not line.__contains__(']'):
-                urls.append(line)
-            else:
-                line = line.replace(']', '')
-                urls.append(line)
-
         if not os.path.exists(package_dir):
             raise CommandError('Given package %s doesn\'t exist!' % package)
+
+        api_skeleton = open(os.path.join(akoikelov.djazz.__path__[0], 'conf', ) + '/tpl/api.py-tpl').read()
+        api_file_resource = open(package_dir + '/api.py', 'a')
+        urls = ''
+        urls_skeleton = "\nurlpatterns.append(url(r'^api/', include(%sResource().urls)))"
 
         for m in models_names:
             generator = ApiGenerator(m, api_file_resource, api_skeleton)
             generator.generate()
-            urls.append(urls_skeleton % m)
+            urls += urls_skeleton % m
 
-        urls.append(']\n')
-
-        for i in urls:
-            url_data += i
-
-        urls_write_file_resource = open(os.path.join(os.getcwd(), main_package_name) + '/urls.py', 'w')
-        urls_write_file_resource.write(url_data)
+        urls_write_file_resource = open(os.path.join(os.getcwd(), main_package_name) + '/urls.py', 'a')
+        urls_write_file_resource.write('\n' + urls)
         urls_write_file_resource.close()
         api_file_resource.close()
 
