571a4e0344bd1557134190162cff07ed2b32ad67
Aykol Koykelov <kolyakoikelov@gmail.com>
2018-05-04 15:03:13 +0600

    ^ changed version in setup.py

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ for dirpath, dirnames, filenames in os.walk(extensions_dir):
 
 setup(
     name='akoikelov.djazz',
-    version='0.0.3',
+    version='0.0.5',
     packages=packages,
     package_data=package_data,
     include_package_data=True,
