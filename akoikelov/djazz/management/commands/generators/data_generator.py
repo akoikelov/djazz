8775c92c6b78860290fe2325edbd7873a329cf42
Aykol Koykelov <kolyakoikelov@gmail.com>
2018-10-27 17:55:11 +0600

    - deleted gen_data command

--- akoikelov/djazz/management/commands/generators/data_generator.py
+++
@@ -1,9 +0,0 @@
-
-from django.db.models.fields import *
-
-
-class DataGenerator(object):
-
-    def generate(self, models):
-        for m in models:
-            fields = [i for i in m._meta.fields]
